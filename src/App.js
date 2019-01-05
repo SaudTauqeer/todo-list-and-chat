89913f080afbba97e218e14f5102618c62f0b900
Saud Tauqeer <saudtauqeer381@gmail.com>
2019-01-05 16:10:15 +0500

    Components -> Basic navbar.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //Imports
 import React, { Component } from 'react';
+import 'typeface-roboto';
 import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
 //Router Components
 import LandingPage from "./Components/LandingPage";
--- src/Components/LandingPage/Navbar.js
+++ src/Components/LandingPage/Navbar.js
@@ -1,13 +1,24 @@
 //Imports
 import React from "react";
-import {Grid, AppBar, Typography, Toolbar} from "@material-ui/core";
+import {Grid, Typography, Toolbar} from "@material-ui/core";
 
 
 const Navbar = () =>(
     <React.Fragment>
-        <AppBar>
-            <Toolbar>ss</Toolbar>
-        </AppBar>
+        <Toolbar>
+            <Grid container justify="space-between">
+                <Grid item > 
+                    <Typography variant="h5">
+                        Todo-List & Chat
+                    </Typography>
+                </Grid>
+                <Grid item >
+                    <Typography>
+                        Hello there 
+                    </Typography>
+                </Grid>
+            </Grid>
+        </Toolbar>
     </React.Fragment>
 );
 
